9870ab23d6d621c5d213ba2520685186beecd380
anhnn.apd@gmail.com <anhnn.apd@gmail.com>
2024-03-31 16:55:40 +0700

    done bài tập Broswer Command

--- selenium/webdriver/Topic_06_WebBrowser_Commands_Ex.java
+++ selenium/webdriver/Topic_06_WebBrowser_Commands_Ex.java
@@ -16,6 +16,10 @@ public class Topic_06_WebBrowser_Commands_Ex {
     String projectPath = System.getProperty("user.dir");
     String osName = System.getProperty("os.name");
 
+//    String urlStep01 = "http://live.techpanda.org/";
+//    String registerPage = "http://live.techpanda.org/customer/account/create/";
+//    String loginPage = "http://live.techpanda.org/index.php/customer/account/login/";
+
     @BeforeClass
     public void BeforeClass(){
         if (osName.contains("Window")){
@@ -28,6 +32,7 @@ public class Topic_06_WebBrowser_Commands_Ex {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(300));
 //        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+
 
     }
     @Test
@@ -61,6 +66,66 @@ public class Topic_06_WebBrowser_Commands_Ex {
         //tương tự Step 03
         Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
 
+    }
+    @Test
+    public void TC_02_Verify_Title(){
+        //Step 01: truy cập trang http://live.techpanda.org/
+        driver.get("http://live.techpanda.org/");
+
+        //Step 02: click MY ACCOUNT link tại footer
+        driver.findElement(By.xpath("//div[@class = 'footer']//a[@title = 'My Account']")).click();
+
+        //Step 03: Verify title của Login Page = Customer Login
+        Assert.assertEquals(driver.getTitle(),"Customer Login");
+
+        //Step 04: Click CREATE AN ACCOUNT button
+        driver.findElement(By.xpath("//a[@title = 'Create an Account']")).click();
+
+        //Step 05: Verify title của Register Page = Create New Customer Account
+        Assert.assertEquals(driver.getTitle(), "Create New Customer Account");
+
+    }
+    @Test
+    public void TC_03_Navigate_Function(){
+        //Step 01: truy cập vào trang
+        driver.get("http://live.techpanda.org/");
+        //Step 02: Click MY ACCOUNT link tại footer
+        driver.findElement(By.xpath("//div[@class = 'footer']//a[@title = 'My Account']")).click();
+
+        //Step 03: Click CREATE AN ACCOUNT button
+        driver.findElement(By.xpath("//a[@title = 'Create an Account']")).click();
+
+        //Step 04: Verify Url của Register Page = http://live.techpanda.org/index.php/customer/account/create/
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
+
+        //Step 05: Back lại trang Login Page
+        driver.navigate().back();
+
+        //Step 06: Verify url của Login Page = http://live.techpanda.org/index.php/customer/account/login/
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/login/");
+
+        //Step 07: Forward tới trang Register Page
+        driver.navigate().forward();
+
+        //Step 08: Verify title của Register Page = Create New Customer Account
+        Assert.assertEquals(driver.getTitle(), "Create New Customer Account");
+    }
+    @Test
+    public void TC_04_Get_Page_Source_Code(){
+        //Step 01: truy cập vào trang http://live.techpanda.org/
+        driver.get("http://live.techpanda.org/");
+
+        //Step 02 Click MY ACCOUNT link tại footer
+        driver.findElement(By.xpath("//div[@class = 'footer']//a[@title = 'My Account']")).click();
+
+        //Step 03: Verify Login Page chứa text "Login or Create an Account"
+        Assert.assertTrue(driver.getPageSource().contains("Login or Create an Account"));
+
+        //Step 04: Click CREATE AN ACCOUNT button
+        driver.findElement(By.xpath("//a[@title = 'Create an Account']")).click();
+
+        //Step 05: Verify Register Page chứa text "Create an Account"
+        Assert.assertTrue(driver.getPageSource().contains("Create an Account"));
     }
     @AfterClass
     public void afterClass() {
